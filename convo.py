2cee718f8c2a51331a88fb05dfb6042b8e7cfde9
Richard Hydomako <rhydomako@gmail.com>
2015-02-13 02:19:13 +0000

    moved data augmenting into a BatchIterator class

--- convo.py
+++ convo.py
@@ -2,12 +2,15 @@ import time
 import numpy as np
 import pandas as pd
 import sys
+import random
+import math
 
+from scipy.ndimage.interpolation import affine_transform
 from sklearn.utils import shuffle
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from lasagne.nonlinearities import softmax
-from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import NeuralNet, BatchIterator
 
 try:
     import pylearn2
@@ -45,6 +48,32 @@ def load():
 	return X_train,y_train,X_test
 
 
+class ImageTransformationIterator(BatchIterator):
+	def transform(self, Xb, yb):
+		Xb, yb = super(ImageTransformationIterator, self).transform(Xb, yb)
+
+		for i,img in enumerate(Xb[:,0,:,:]):
+
+            		c_in = 0.5*np.array(img.shape)
+            		c_out = np.array((14.0+random.randint(-1,1),14.0+random.randint(-1,1)))
+
+            		theta = 20*(random.random()-0.5)*math.pi/180.0
+            		scaling = np.diag(([1.+0.4*(random.random()-0.5),
+                        	            1.+0.4*(random.random()-0.5)]))
+
+            		transform = np.array([[math.cos(theta),-math.sin(theta)],
+                        	              [math.sin(theta), math.cos(theta)]]).dot(scaling)
+            		offset=c_in-c_out.dot(transform)
+            		dst=affine_transform(
+                    		img,transform.T,order=2,offset=offset,output_shape=(28,28),cval=0.0,output=np.float32
+            		)
+            		dst[dst<0.01] = 0.
+
+			Xb[i,0]=dst
+
+		return Xb, yb
+
+
 net = NeuralNet(
     layers=[
         ('input', layers.InputLayer),
@@ -52,27 +81,31 @@ net = NeuralNet(
         ('pool1', MaxPool2DLayer),
 	('dropout1', layers.DropoutLayer),
         ('conv2', Conv2DLayer),
-        ('pool2', MaxPool2DLayer),
-	('dropout2', layers.DropoutLayer),
-   #     ('conv3', Conv2DLayer),
-   #     ('pool3', MaxPool2DLayer),
-#	('dropout3', layers.DropoutLayer),
-        ('hidden4', layers.DenseLayer),
+	('conv4', Conv2DLayer),
+        ('pool4', MaxPool2DLayer),
 	('dropout4', layers.DropoutLayer),
-        ('hidden5', layers.DenseLayer),
+        ('conv5', Conv2DLayer),
+	('conv7', Conv2DLayer),
+        ('pool7', MaxPool2DLayer),
+	('dropout7', layers.DropoutLayer),
+        ('hidden8', layers.DenseLayer),
+	('dropout9', layers.DropoutLayer),
+        ('hidden10', layers.DenseLayer),
         ('output', layers.DenseLayer),
         ],
     # layer parameters:
     input_shape=(None, 1, 28, 28),  # 28x28 input pixels per batch
-    conv1_num_filters=32, conv1_filter_size=(5, 5), pool1_ds=(2, 2),
-    dropout1_p=0.1,
-    conv2_num_filters=32, conv2_filter_size=(5, 5), pool2_ds=(2, 2),
-    dropout2_p=0.2,
-#    conv3_num_filters=32, conv3_filter_size=(5, 5), pool3_ds=(2, 2),
-#    dropout3_p=0.3,
-    hidden4_num_units=512,
-    dropout4_p=0.5,
-    hidden5_num_units=512,
+    conv1_num_filters=64, conv1_filter_size=(6, 6), pool1_ds=(2, 2),
+    dropout1_p=0.2,
+    conv2_num_filters=128, conv2_filter_size=(2, 2),
+    conv4_num_filters=128, conv4_filter_size=(2, 2), pool4_ds=(2, 2),
+    dropout4_p=0.3,
+    conv5_num_filters=256, conv5_filter_size=(2, 2),
+    conv7_num_filters=256, conv7_filter_size=(2, 2), pool7_ds=(2, 2),
+    dropout7_p=0.3,
+    hidden8_num_units=2048,
+    dropout9_p=0.6,
+    hidden10_num_units=4096,
 
     output_nonlinearity=softmax,  # !
     output_num_units=10,  # 10 target values
@@ -82,7 +115,9 @@ net = NeuralNet(
     update_learning_rate=0.01,
     update_momentum=0.9,
 
-    max_epochs=100,  # we want to train this many epochs
+    batch_iterator_train=ImageTransformationIterator(batch_size=128),
+
+    max_epochs=2000,  # we want to train this many epochs
     verbose=1,
     )
 
@@ -106,6 +141,6 @@ with open("convo.predictions."+ts,"w") as f:
         ImageId = ImageId + 1
 
 #pickle model
-import cPickle as pickle
-with open('convo.pickle.'+ts, 'wb') as f:
-    pickle.dump(net, f, -1)
+#import cPickle as pickle
+#with open('convo.pickle.'+ts, 'wb') as f:
+#    pickle.dump(net, f, -1)
